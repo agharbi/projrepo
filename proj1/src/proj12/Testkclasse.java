3b226a915f4f62e9300d80ba09ff202b061fce31
26saida <26saida@26saida-PC.lan>
2013-05-26 12:08:24 +0200

    Testklasse erstellen

---
+++ proj1/src/proj12/Testkclasse.java
@@ -0,0 +1,14 @@
+package proj12;
+
+// Testclasse erstellen
+public class Testkclasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
